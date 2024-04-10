b0e3544d55a9028b61a428b0b7d3d61ead4ade96
Yue Zhao <yzhao@duck.com>
2024-04-10 16:18:44 +0200

    add padding dynamically based on the max seq_length of the batch

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -2,19 +2,22 @@ import torch
 from torch.utils.data import DataLoader, TensorDataset
 import os
 import pandas as pd
+from torch.nn.utils.rnn import pad_sequence
 
 def load_dataloader(config, device):
     data_dir = config['data']['data_dir']    
     train_dataset = load_data(data_dir, config['data']['train_file'])    
     val_dataset = load_data(data_dir, config['data']['val_file'])    
     test_dataset = load_data(data_dir, config['data']['test_file'])    
+    test_helper_dataset = load_data(data_dir, config['data']['test_helperfile']) # hit_ids and event_ids 
 
     batch_size = config['training']['batch_size']
     shuffle = config['training']['shuffle']
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle)
-    val_loader = DataLoader(val_dataset, batch_size=batch_size)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size)
-    return train_loader, val_loader, test_loader
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle, collate_fn=collate_fn)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, collate_fn=collate_fn)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, collate_fn=collate_fn)
+    test_helper_loader = DataLoader(test_helper_dataset, batch_size=batch_size) # padding not necessary here
+    return train_loader, val_loader, test_loader, test_helper_loader
 
 def load_data(data_dir, file_name):
     file_path = os.path.join(data_dir, file_name)
@@ -25,3 +28,10 @@ def load_truths(config):
     test_truth_filename = config['data']['test_truthfile']
     test_truth_filepath = os.path.join(data_dir, test_truth_filename)
     return pd.read_csv(test_truth_filepath)
+
+# dynamically padding data based on the max seq_length of the batch
+def collate_fn(batch):
+    coords, labels = zip(*batch)
+    coords_padded = pad_sequence(coords, batch_first=True, padding_value=0.0)
+    labels_padded = pad_sequence(labels, batch_first=True, padding_value=-1)
+    return coords_padded, labels_padded 
